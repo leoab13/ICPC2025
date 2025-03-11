694c578cb8ee5d8273ad5e122edb6f81221e3af9
leoab13 <l21090939@queretaro.tecnm.mx>
2025-03-10 18:56:47 -0600

    ok

---
+++ STDL/Distinct Count.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+string sol(vector<int> &n)
+{
+    int t = n.size() - 1;
+    int x = n[t];
+    n.pop_back();
+    set<int> s;
+    for(int a : n)
+    {
+        s.insert(a);
+    }
+    int r = s.size();
+    if(r == x) 
+        return "Good";
+    else if(r > x)
+        return "Average";
+    return "Bad";
+}
+
+
+int main()
+{
+    int t;
+    vector<vector<int>> nums;
+    vector<int> tmp;
+    cin >> t;
+    for(int i = 0; i < t; i++)
+    {
+        int n ;
+        cin >> n;
+        int x;
+        cin >> x;
+        int tp;
+        for(int i = 0; i < n ; i ++)
+        {
+            cin >> tp;
+            tmp.push_back(tp);
+        }
+        tmp.push_back(x);
+        nums.push_back(tmp);
+        tmp.clear();
+    }
+    for(vector<int> k : nums)
+    {
+        cout << sol(k) << endl;
+    }
+    return 0;
+}
--- STDL/Sereja and Dima.cpp
+++ STDL/Sereja and Dima.cpp
@@ -1,33 +1,43 @@
+//https://codeforces.com/contest/381/problem/A
 #include <bits/stdc++.h>
 using namespace std;
 
-void sol(set<int>& cards, int x)
+
+//SOLUCION CON DOS PUNTEROS
+void sol(vector<int> & cards)
 {
-    vector<int> ans(2, 0);
-    
-    for (int j = 0; j < x; j++)
+    int n = cards.size();
+    vector<int> ans = {0,0};
+    int c = 0, d = 1;
+    int a = cards[c];
+    int b = cards[n -d];
+    int m;
+    for(int i = 0; i < n; i ++)
     {
-        auto last = prev(cards.end());
-        ans[j % 2] += *last;
-        cards.erase(last);
+        m = max(a,b);
+        ans[i%2] += m;
+        if(m == a)
+        {
+            a = cards[++c];
+        }
+        else{
+            b = cards[n - (++d)];
+        }
     }
-    
-    cout << ans[0] << " " << ans[1] << endl;
+    cout << ans[0] << " " << ans[1];
 }
 
 int main()
 {
     int x;
     cin >> x;
-    set<int> cards;
-    
-    for (int i = 0; i < x; i++)
+    vector<int> cards;
+    int tmp;
+    for(int i = 0; i < x; i++)
     {
-        int tmp;
         cin >> tmp;
-        cards.insert(tmp);
+        cards.push_back(tmp);
     }
-
-    sol(cards, x);
+    sol(cards);
     return 0;
 }
