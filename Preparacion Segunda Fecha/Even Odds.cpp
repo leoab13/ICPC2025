d3a86034914cb532dd0be5db2d977e9a607fc03f
leoab13 <l21090939@queretaro.tecnm.mx>
2025-05-19 16:13:28 -0600

    update 2da fecha

---
+++ Preparacion Segunda Fecha/Even Odds.cpp
@@ -0,0 +1,13 @@
+//https://codeforces.com/problemset/problem/318/A
+#include <bits/stdc++.h>
+using namespace std;
+
+void sol(vector<int> &nums, int &k)
+{
+//si el k es menor a el tamano/2 es impar si es mayor es par
+}
+
+int main()
+{
+    return 0;
+}
---
+++ Preparacion Segunda Fecha/Xenia and Ringroad.cpp
@@ -0,0 +1,7 @@
+//https://codeforces.com/problemset/problem/339/B
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    return 0;
+}
